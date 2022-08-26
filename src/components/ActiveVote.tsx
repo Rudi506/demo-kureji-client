2ac0f3e58b23c55bb9fea1c352543982293e9b8c
Agung Firmansyah <agungfir98@gmail.com>
2022-08-26 18:23:53 +0700

    UI Adjustment

--- src/components/ActiveVote.tsx
+++ src/components/ActiveVote.tsx
@@ -58,39 +58,18 @@ export const VoteEvent: React.FC = () => {
                     </p>
                   </div>
 
-                  <p className="text-xs flex gap-1">
+                  <div className="text-xs flex gap-1">
                     kandidat:
                     {v.candidates.map((v, i) => (
-                      <p className="even:before:content-['_vs_']">
+                      <p key={i} className="even:before:content-['_vs_']">
                         {v.calonKetua} &amp; {v.calonWakil}
                       </p>
                     ))}
-                    {/* kandidat: andrew &amp; bob vs jude &amp; anna */}
-                  </p>
+                  </div>
                 </div>
               </Link>
             </li>
           ))}
-          {/* <li>
-            <div
-              id="card"
-              className="border-2 border-gray-300 rounded-lg shadow-md shadow-gray-500 px-2 py-2 flex flex-col gap-2 relative"
-            >
-              <div className="absolute right-2 bg-gray-400 px-1 rounded-md">
-                <p className="text-xs text-white/90 font-semibold">
-                  sudah Vote
-                </p>
-              </div>
-              <div className="">
-                <h1 className="text-lg font-semibold">Pemilihan ketua ROHIS</h1>
-                <p className="text-gray-500 text-xs">SMK N 1 Purbalingga</p>
-              </div>
-
-              <p className="text-xs">
-                kandidat: andrew &amp; bob vs jude &amp; anna
-              </p>
-            </div>
-          </li> */}
         </ul>
       </div>
     </>
