32bb6dcad1126fc3c1d29b421914e102e9f2f623
Agung Firmansyah <agungfir98@gmail.com>
2022-09-05 10:54:30 +0700

    add list component

---
+++ src/components/ListComponent.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export const ListComponent: React.FC<{ key: number; children: ReactNode }> = ({
+  key,
+  children,
+}) => {
+  return (
+    <li key={key} className={`odd:bg-slate-200 p-3`}>
+      {children}
+    </li>
+  );
+};
