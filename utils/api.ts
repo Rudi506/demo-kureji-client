e955c2c0fc07f3138f10497b16444e1f9ff6163d
Rudi506 <rudisakti332@gmail.com>
2022-10-16 21:09:19 +0700

    new

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://pilketosserver.herokuapp.com`,
   withCredentials: true,
 });
 // this file is curently ignored in gitignore
