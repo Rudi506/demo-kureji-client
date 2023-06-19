2d755a61958ce98306425e9deee9ab3022cdeba0
agungfir98 <agungfir98@gmail.com>
2023-06-19 10:59:27 +0700

    change URI to current backend

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `https://demo-kureji.herokuapp.com`,
+  // baseURL: ``demo-kureji-server-production.up.railway.app`,
+  baseURL: `https://demo-kureji-server-production.up.railway.app`,
   withCredentials: true,
 });
 // this file is curently ignored in gitignore
