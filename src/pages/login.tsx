ee73fbb711eaea275ac6dafb5580d9ec8697936b
agungfir98 <agungfir98@gmail.com>
2023-06-25 22:38:25 +0700

    login UI

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,55 +1,63 @@
+import { Button, Form, Input } from "antd";
 import { useState } from "react";
-import LoginForm from "../components/loginForm";
-import Register from "../components/regisForm";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { api } from "../../utils/api";
+import { setAccessToken } from "../utils/accesstoken";
 
 function Login() {
-  const [inOrup, setInOrUp] = useState("login");
+	const navigate = useNavigate();
 
-  const renderForm = () => {
-    if (inOrup === "login") {
-      return <LoginForm />;
-    }
-    if (inOrup === "signup") {
-      return <Register />;
-    }
-  };
+	const [loading, setLoading] = useState<boolean>();
+
+	const redirect = () => {
+		navigate('/signup');
+	};
+
+	const handleSubmit = async(values: {email: string, password: string}) => {
+
+    return api
+      .post("/login", values)
+      .then((result) => {
+        if (result && result.data) setAccessToken(result.data.token);
+        navigate("/");
+				toast.success('Berhasil login');
+        setLoading(false);
+      })
+      .catch((err) => {
+				console.log(err);
+        setLoading(false);
+      });
+	};
 
   return (
     <>
-      <div className="container min-h-screen mx-auto md:flex justify-between font-inter">
-        <div className="hidden md:grow md:flex align-middle w-3/4 lg:w-auto">
-          <img
-            className="max-h-screen w-auto"
-            src="votebg.svg"
-            alt="Vote Img"
-          />
-        </div>
-        <div className="relative max-w-md sm:self-end md:w-3/4 align my-auto h-screen ">
-          <h1 className="text-4xl mx-auto w-fit font-semibold py-10">
-            Demo-kureji
-          </h1>
-          <div id="form-wrapper">
-            <div id="loginregis" className="flex justify-around">
-              <button
-                onClick={() => setInOrUp("login")}
-                className={`font-semibold w-full rounded-t-xl  py-2 bg-gray-200 text-gray-400 ${
-                  inOrup === "login" && "  bg-gray-500 text-white/100"
-                }`}
-              >
-                <h2 className="text-2xl">Login</h2>
-              </button>
-              <button
-                onClick={() => setInOrUp("signup")}
-                className={`font-semibold w-full rounded-t-xl  py-2 bg-gray-200 text-gray-400 ${
-                  inOrup === "signup" && "  bg-gray-500 text-white/100"
-                }`}
-              >
-                <h2 className="text-2xl">Sign Up</h2>
-              </button>
-            </div>
-            {renderForm()}
-          </div>
-        </div>
+      <div className="min-h-screen mx-auto md:flex justify-center font-inter bg-gradient-to-tr from-sky-900 via-sky-700 to-fuchsia-600">
+				<div className="container self-center flex justify-center">
+					<div className="flex w-3/4 rounded-xl overflow-hidden drop-shadow-2xl " >
+						<div id="left" className="w-full bg-white flex flex-col items-center py-10 justify-center">
+							<h1 className="text-2xl font-medium">Sign In</h1>
+							<div className="w-1/2">
+								<Form onFinish={handleSubmit} layout="vertical" className="flex flex-col">
+									<Form.Item name="email" label="email" required>
+										<Input placeholder="User Email" />
+									</Form.Item>
+									<Form.Item name="password" label="password" required>
+										<Input.Password autoComplete="new-password" placeholder="User Email" />
+									</Form.Item>
+									<Button type="primary" loading={loading} className="bg-blue-400 mt-4 font-medium" size="large" htmlType="submit" shape="round">Sign In</Button>
+								</Form>
+							</div>
+						</div>
+						<div id="right" className="w-full bg-blue-500 flex flex-col justify-center items-center gap-3">
+							<div className="text-white font-medium text-center flex flex-col gap-3">
+								<h1 className="text-2xl">Sign Up</h1>
+								<p className="text-sm">Sign Up here if you don't have an account</p>
+							</div>
+							<Button shape="round" type="default" className="bg-white text-blue-500 font-bold" size="large" onClick={redirect}>Sign Up</Button>
+						</div>
+					</div>
+				</div>
       </div>
     </>
   );
