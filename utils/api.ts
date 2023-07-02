b23410b0e3ea877ad1645833b7d5b21bb0d2ba7c
Rudi506 <rudisakti332@gmail.com>
2023-07-02 13:58:17 +0700

    uti;ls

--- utils/api.ts
+++ utils/api.ts
@@ -1,16 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
-<<<<<<< HEAD
   // baseURL: ``demo-kureji-server-production.up.railway.app`,
-  baseURL: `https://demo-kureji-server-production.up.railway.app`,
-=======
-  baseURL: `http://localhost:3000`,
->>>>>>> 8bb40b2 (change login interface)
-=======
-  baseURL: `https://pilketosserver.herokuapp.com`,
->>>>>>> e955c2c0fc07f3138f10497b16444e1f9ff6163d
+  baseURL: `https://demo-kureji-server-production-822a.up.railway.app/`,
   withCredentials: true,
 });
 // this file is curently ignored in gitignore
